c0736de84e5d63c098bcc53eee3aee5d5cc523ab
pincky6 <mentovsky4@gmail.com>
2023-11-21 01:45:49 +0300

    add DateTaskDetailFragment and DateTaskViewModel

--- app/src/main/java/com/diplom/todoapp/dialogs/fragments/DateTaskDetailFragment.java
+++ app/src/main/java/com/diplom/todoapp/dialogs/fragments/DateTaskDetailFragment.java
@@ -1,52 +1,96 @@
 package com.diplom.todoapp.dialogs.fragments;
 
+import static androidx.navigation.ViewKt.findNavController;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.diplom.todoapp.R;
-import com.diplom.todoapp.databinding.FragmentTaskDetailBinding;
+import com.diplom.todoapp.databinding.FragmentDateTaskDetailBinding;
+import com.diplom.todoapp.dialogs.viewmodels.DateTaskDetailViewModel;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class DateTaskDetailFragment extends Fragment {
-    FragmentTaskDetailBinding binding = null;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
+public class DateTaskDetailFragment extends AbstractTaskDetailFragment {
+
+    private DateTaskDetailViewModel dateTaskDetailViewModel;
+    public FragmentDateTaskDetailBinding binding = null;
+    public static final String DATE_TASK_KEY = "DATE_TASK_KEY";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dateTaskDetailViewModel = new DateTaskDetailViewModel();
     }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentTaskDetailBinding.inflate(inflater, container, false);
-
-//        String[] reminders = new String[]{"1 day before", "5 minutes before", "Don\'t remind"};
-//        ArrayAdapter<String> remindAdapter = new ArrayAdapter<>(getContext(), R.layout.fragment_task_detail, reminders);
-//        binding.taskRemind.setAdapter(remindAdapter);
-//
-//        String[] priorities = new String[]{"Low", "Middle", "High"};
-//        ArrayAdapter<String> prioritiyAdapter = new ArrayAdapter<>(getContext(), R.layout.fragment_task_detail, priorities);
-//        binding.taskRemind.setAdapter(prioritiyAdapter);
-
-
-
+        binding = FragmentDateTaskDetailBinding.inflate(inflater, container, false);
+        initTextWatchers(binding.dateTaskTitle, binding.dateTaskDescribe,
+                binding.dateTaskPlace, binding.dateTaskEditTextDate,
+                binding.dateTaskEditTextTime, binding.dateTaskEditTextDate2,
+                binding.dateTaskEditTextTime2);
+        initSpinners(binding.dateTaskReminder, binding.dateTaskPriority);
+        initToolbar(binding.toolbar);
+        initDateInputs(binding.dateTaskEditTextDate,
+                        binding.dateTaskEditTextDate2);
+        initTimeInputs(binding.dateTaskEditTextTime,
+                binding.dateTaskEditTextTime2);
+        initSaveButton();
         return binding.getRoot();
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+        private void initSaveButton(){
+        binding.dateTaskSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    dateTaskDetailViewModel.setTask(binding);
+                }
+                catch (IllegalArgumentException e){
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+                catch (IOException e){
+                    setErrorBackground(binding.dateTaskTitle, binding.dateTaskDescribe,
+                            binding.dateTaskPlace, binding.dateTaskEditTextDate,
+                            binding.dateTaskEditTextTime, binding.dateTaskEditTextDate2,
+                            binding.dateTaskEditTextTime2);
+                    return;
+                }
+                Bundle bundle = new Bundle();
+                bundle.putSerializable(DATE_TASK_KEY, dateTaskDetailViewModel.getDateTask());
+                getParentFragmentManager().setFragmentResult(DATE_TASK_KEY, bundle);
+                findNavController(getView()).navigate(
+                        DateTaskDetailFragmentDirections.actionDateTaskDetailFragmentToEventTaskFragment()
+                );
+            }
+        });
     }
 }
--- app/src/main/java/com/diplom/todoapp/dialogs/viewmodels/DateTaskDetailViewModel.java
+++ app/src/main/java/com/diplom/todoapp/dialogs/viewmodels/DateTaskDetailViewModel.java
@@ -1,4 +1,85 @@
 package com.diplom.todoapp.dialogs.viewmodels;
 
-public class DateTaskViewModel {
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+import com.diplom.todoapp.R;
+import com.diplom.todoapp.databinding.FragmentDateTaskDetailBinding;
+import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.DateTask;
+import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.Priority;
+import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.Reminders;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTaskDetailViewModel {
+    private DateTask dateTask = null;
+    public DateTask getDateTask(){
+        return dateTask;
+    }
+
+    public void setTask(@NonNull FragmentDateTaskDetailBinding binding) throws IOException {
+        if(checkEditors(binding.dateTaskTitle, binding.dateTaskDescribe,
+                        binding.dateTaskPlace, binding.dateTaskEditTextDate,
+                        binding.dateTaskEditTextTime, binding.dateTaskEditTextDate2,
+                        binding.dateTaskEditTextTime2)){
+            throw new IOException();
+        }
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date createDate = null;
+        String title = binding.dateTaskTitle.getText().toString();
+        String place = binding.dateTaskPlace.getText().toString();
+        String describe = binding.dateTaskDescribe.getText().toString();
+        boolean allDay = binding.allDayCheckBox.isChecked();
+        Date dateStart = null;
+        Date dateEnd = null;
+        Priority priority = getPriority(binding.dateTaskPriority.getSelectedItem().toString());
+        Reminders reminders = getReminder(binding.dateTaskReminder.getSelectedItem().toString());
+        try {
+            createDate = format.parse(format.format(new Date()));
+            dateStart = format.parse(binding.dateTaskEditTextDate.getText().toString());
+            dateEnd = format.parse(binding.dateTaskEditTextDate2.getText().toString());
+            dateTask = new DateTask(createDate, place, title, describe, allDay,
+                    dateStart, dateEnd, priority, reminders);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("wrong date input");
+        }
+    }
+
+    private Priority getPriority(String priority){
+        switch (priority){
+            case "Low":
+                return Priority.LOW;
+            case "Medium":
+                return Priority.MEDIUM;
+            case "HIGH":
+                return Priority.HIGH;
+        }
+        return Priority.LOW;
+    }
+    private Reminders getReminder(String reminder){
+        switch (reminder){
+            case "1 day before":
+                return Reminders.DAY_1_BEFORE;
+            case "5 minutes before":
+                return Reminders.MINUTES_5_BEFORE;
+            case "Don\'t remind":
+                return Reminders.DONT_REMIND;
+        }
+        return Reminders.DONT_REMIND;
+    }
+    private <Editor extends EditText> boolean checkEditors(Editor... editors){
+        for (Editor editor: editors){
+            if(editor.getText().toString().isEmpty()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
