b1b69048452b1004f13b50d3788df855e8f7cdb8
pincky6 <mentovsky4@gmail.com>
2023-11-21 05:31:17 +0300

    add EditorsUtil x2

---
+++ app/src/main/java/com/diplom/todoapp/EditorsUtil.java
@@ -0,0 +1,45 @@
+package com.diplom.todoapp;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class EditorsUtil {
+
+    public static  <Editor extends EditText> boolean checkEditors(Editor... editors){
+        for (Editor editor: editors){
+            if(editor.getText().toString().isEmpty()){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static  <Editor extends EditText> void setErrorBackground(Editor... editors) {
+        for (Editor editor : editors) {
+            if (editor.getText().toString().isEmpty()) {
+                editor.setBackgroundResource(R.drawable.error_background);
+                editor.setError("Empty");
+            }
+        }
+    }
+    public static <Editor extends EditText> void initTextWatchers(Editor... editors) {
+        for (Editor editor : editors)
+            editor.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (!editor.getText().toString().isEmpty()) {
+                        editor.setBackgroundResource(R.drawable.normal_background);
+                        editor.setError(null);
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+    }
+}
