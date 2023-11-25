e7fc6b0414ff0bf0a46dc4359b9a2c11c8f0d26f
pincky6 <mentovsky4@gmail.com>
2023-11-25 21:17:04 +0300

    implement MaterialCalendarView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -47,7 +46,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.5")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.applandeo:material-calendar-view:1.7.0")
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
