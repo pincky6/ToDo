b81a617cbab451da55d911e1880bd0b01b820a5b
pincky6 <mentovsky4@gmail.com>
2023-11-28 23:35:39 +0300

    cleanup

--- app/src/main/java/com/diplom/todoapp/eventtask/TaskViewModel.java
+++ app/src/main/java/com/diplom/todoapp/eventtask/TaskViewModel.java
@@ -1,21 +1,14 @@
 package com.diplom.todoapp.eventtask;
 
-import android.content.Context;
 
 import androidx.lifecycle.ViewModel;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.AbstractTask;
-import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.DateTask;
-import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.Priority;
-import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.Reminders;
-import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.Task;
 import com.diplom.todoapp.firebase.FirebaseRepository;
-import com.google.firebase.database.FirebaseDatabase;
+import com.diplom.todoapp.firebase.InitExpression;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Objects;
 
 public class TaskViewModel extends ViewModel {
@@ -24,29 +17,17 @@ public class TaskViewModel extends ViewModel {
     public TaskViewModel(){
         firebase = FirebaseRepository.getInstance();
         taskList = new ArrayList<>();
-//        Priority[] priorities = {Priority.LOW, Priority.MEDIUM, Priority.HIGH};
-//        Reminders[] reminders = {Reminders.DONT_REMIND, Reminders.MINUTES_5_BEFORE, Reminders.DAY_1_BEFORE};
-//        for(int i = 0; i < 100; i++){
-//            Date date = new Date();
-//            if(i % 2 == 0)
-//            taskList.add(new Task(date, "Task #" + i,
-//                          "this is my task",
-//                    true, date, priorities[i % 3], reminders[i % 3]));
-//            else
-//                taskList.add(new DateTask(date,
-//                        "Task #" + i, "dom",
-//                        "this is my date task",
-//                        false, date,
-//                        date,
-//                        priorities[i % 3], reminders[i % 3]));
-//        }
     }
-    public void loadFirebase(RecyclerView recyclerView){
-        firebase.readAllTasks(taskList, recyclerView);
+    public void loadFirebase(RecyclerView recyclerView, InitExpression initLambda){
+        firebase.readAllTasks(taskList, recyclerView, initLambda);
     }
-    public void add(AbstractTask abstractTask){
-        firebase.addTask(abstractTask);
-        taskList.add(abstractTask);
+    public AbstractTask getFromId(String id){
+        for(AbstractTask abstractTask: taskList){
+            if(Objects.equals(abstractTask.id, id)) {
+                return abstractTask;
+            }
+        }
+        throw new IllegalArgumentException("wrong id");
     }
     public void remove(String id){
         for(AbstractTask abstractTask: taskList){
