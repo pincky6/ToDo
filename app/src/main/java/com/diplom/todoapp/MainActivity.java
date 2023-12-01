d648b0b047ba72428fc018993a94d03d6e0211d2
pincky6 <mentovsky4@gmail.com>
2023-12-01 09:57:42 +0300

    init singletone

--- app/src/main/java/com/diplom/todoapp/MainActivity.java
+++ app/src/main/java/com/diplom/todoapp/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.diplom.todoapp.eventtask.CalendarSingletone;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
