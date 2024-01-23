0264b18d7b4fe008e82bd43fbd8dfd5832a1c49a
pincky6 <mentovsky4@gmail.com>
2024-01-23 07:14:25 +0300

    add new listener

---
+++ app/src/main/java/com/diplom/todoapp/eventtask/listeners/SetSuccsessListener.java
@@ -0,0 +1,7 @@
+package com.diplom.todoapp.eventtask.listeners;
+
+import com.diplom.todoapp.eventtask.eventtaskrecyclerview.models.AbstractTask;
+
+public interface SetSuccsessListener {
+    public void set(AbstractTask abstractTask);
+}
