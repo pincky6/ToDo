4ce19372e4d3193a4a578b25afbd7be3719977c3
pincky6 <mentovsky4@gmail.com>
2024-01-31 00:47:28 +0300

    create class fragment, repo and fragment layout

---
+++ app/src/main/java/com/diplom/todoapp/settings/SettingsFragment.java
@@ -0,0 +1,7 @@
+package com.diplom.todoapp.settings;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/diplom/todoapp/settings/SettingsRepository.java
@@ -0,0 +1,4 @@
+package com.diplom.todoapp.settings;
+
+public class SettingsRepository {
+}
