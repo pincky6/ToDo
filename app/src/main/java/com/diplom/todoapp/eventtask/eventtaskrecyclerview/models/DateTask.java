60b0df2a85d27e6c27f3730b99c6460a35fef19a
pincky6 <mentovsky4@gmail.com>
2024-02-16 01:17:51 +0300

    add category string into DateTask

--- app/src/main/java/com/diplom/todoapp/eventtask/eventtaskrecyclerview/models/DateTask.java
+++ app/src/main/java/com/diplom/todoapp/eventtask/eventtaskrecyclerview/models/DateTask.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 public class DateTask extends AbstractTask{
     public String place;
+    public String category;
     public Date dateEnd;
 
     public DateTask(){
@@ -17,19 +18,21 @@ public class DateTask extends AbstractTask{
     public  DateTask(@NonNull String id, @NonNull String title,@NonNull String place,
                      @NonNull String describe, boolean allDayFlag,
                      @NonNull Date dateStart, @NonNull Date dateEnd, @NonNull String priority, @NonNull String reminder,
-                     @NonNull String succsessFlag){
+                     @NonNull String succsessFlag, @NonNull String category){
         super(id, title, describe,
                 allDayFlag, dateStart,
                 priority, reminder, succsessFlag);
         this.place = place;
         this.dateEnd = super.safeDateAfterInit(dateEnd, dateStart);
+        this.category = category;
     }
     public void setTask(@NonNull String id, @NonNull String title,@NonNull String place,
                   @NonNull String describe, boolean allDayFlag,
                   @NonNull Date dateStart, @NonNull Date dateEnd, @NonNull String priority, @NonNull String reminder,
-                        @NonNull String succsessFlag){
+                        @NonNull String succsessFlag, @NonNull String category){
         super.setTask(id, title, describe, allDayFlag, dateStart, priority, reminder, succsessFlag);
         this.place = place;
         this.dateEnd = super.safeDateAfterInit(dateEnd, dateStart);
+        this.category = category;
     }
 }
